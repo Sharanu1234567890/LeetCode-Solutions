96cbd045e48c4d5c581fdb5cb260575feeaa570d
Sharanu Malipatil <malipatils249@gmail.com>
2025-06-20 00:47:18 +0530

    Update 2294. Partition Array Such That Maximum Difference Is K.java

--- 2294. Partition Array Such That Maximum Difference Is K.java
+++ 2294. Partition Array Such That Maximum Difference Is K.java
@@ -1,3 +1,14 @@
+// # Partition Array Such That Maximum Difference Is K
+
+// ## Problem Summary
+// Given an integer array `nums` and an integer `k`, partition it into the minimum number of subsequences such that the difference between the maximum and minimum values in each subsequence is at most `k`.
+
+// ## My Thought Process
+
+// - I sorted the array to make the difference between nearby numbers small.
+// - Then I greedily grouped consecutive values with difference <= k.
+// - If the next value is too far from the start of the group, I start a new group.
+
 class Solution {
     public int partitionArray(int[] nums, int k) {
         Arrays.sort(nums);
